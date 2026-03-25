5c597989cb18929b47e193163c60f00d37e6f1d3
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 14:18:49 +0000

    replicar la vista publica de Liga

---
+++ entregas/ponceKevin/src/Liga.java
@@ -0,0 +1,25 @@
+public class Liga {
+    public Liga(String nombre) {
+    
+    }
+
+    public void mostrarCalificacion() {
+    
+    }
+
+    public void listarPartidos() {
+    
+    }
+
+    public void mostarPendientes() {
+    
+    }
+
+    public void registrarPartido(Partido partido) {
+    
+    }
+
+    public void registrarResultado() {
+    
+    }
+}
