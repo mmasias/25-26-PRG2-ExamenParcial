1f4f927d764af0b5954bcbd9527311e176a254aa
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-03-25 16:30:28 +0100

    Agregar clase Equipo de mi examen con constructores y métodos básicos

---
+++ entregas/hoyosFelipe/src/Partido.java
@@ -0,0 +1,35 @@
+package vistas.liga;
+
+class Equipo {
+
+    public Equipo(String nombre, String color, int numeroJugadores)
+
+    public Equipo(String nombre, String color)
+
+    public Equipo()
+
+    public Equipo(Equipo equipo)
+
+    public Equipo clone()
+
+    public String nombre()
+
+    public String color()
+
+    public int numeroJugadores()
+
+    public int puntos()
+
+    public boolean equals(Equipo equipo)
+
+    public boolean tieneMismoNombre(String nombre)
+
+    public void sumarPuntos(int puntos)
+
+    public void reiniciarPuntos()
+
+    public String toString()
+
+    public void mostrar()
+
+}
