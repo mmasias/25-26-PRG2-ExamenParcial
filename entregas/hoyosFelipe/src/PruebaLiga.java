74f8620267fad032d3edaffe2da53b87cdd6a779
Felipe <felipehoyos2008@gmail.com>
2026-03-25 19:05:20 +0100

    Agregar método main en la clase PruebaLiga para inicializar la liga

--- entregas/hoyosFelipe/src/PruebaLiga.java
+++ entregas/hoyosFelipe/src/PruebaLiga.java
@@ -1,5 +1,9 @@
 package vistas.liga;
 
 public class PruebaLiga {
-    
+    public static void main(String[] args) {
+        Liga liga = new Liga("Liga Universitaria Santander 2024/25");
+
+        
+    }
 }
