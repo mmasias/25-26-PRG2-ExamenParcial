a0003ed92536d9738114cda7a0a3697c8aeaeb12
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 13:45:00 +0000

    fix: add assertions to validate non-negative values in Equipo methods

--- entregas/ponceKevin/src/Equipo.java
+++ entregas/ponceKevin/src/Equipo.java
@@ -14,23 +14,32 @@ public class Equipo {
 
     public Equipo(String nombre,String color,int jugadores) {
         this(nombre,color);
+        assert esNoNegativo(jugadores) : "jugadores no puede ser negativo";
         this.jugadores = jugadores;
     }
 
     public void aumentarVictorias(int victorias) {
+        assert esNoNegativo(victorias) : "victorias no puede ser negativo";
         this.victorias += victorias;
     }
 
     public void aumentarDerrotas(int derrotas) {
+        assert esNoNegativo(derrotas) : "derrotas no puede ser negativo";
         this.derrotas += derrotas;
 
     }
     public void aumentarEmpates(int empates) {
+        assert esNoNegativo(empates) : "empates no puede ser negativo";
         this.empates += empates;
     }
 
     public void actualizarTotal(int puntos) {
+        assert esNoNegativo(puntos) : "puntos no puede ser negativo";
         this.total += puntos;
+    }
+
+    private boolean esNoNegativo(int valor) {
+        return valor >= 0;
     }
 
     public String toString() {
