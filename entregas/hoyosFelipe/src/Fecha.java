72d73108f36492cb1b3bc1eacb65de4776ecbc0c
Felipe <felipehoyos2008@gmail.com>
2026-03-25 19:36:05 +0100

    Agregar clase Fecha con constructores y métodos básicos (La he agregado ya que es uportuna y necedaria :)  )

---
+++ entregas/hoyosFelipe/src/Fecha.java
@@ -0,0 +1,25 @@
+class Fecha {
+    
+    public Fecha(int dia, int mes, int año) {}
+
+    public Fecha(int dia, int mes) {}
+
+    public Fecha(){}
+
+    public Fecha(Fecha fecha){}
+
+    public Fecha clone(){}
+
+    public int dia(){}
+
+    public int mes(){}
+    
+    public int año(){}
+
+    public boolean equals(Fecha fecha){}
+
+    public String toString(){}
+
+    public void mostrar(){}
+    
+}
