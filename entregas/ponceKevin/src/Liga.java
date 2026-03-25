6c35accc3404a9456a4aa672633f4e86646b94fd
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 14:28:37 +0000

    refactor: improve output formatting in listarPartidos and mostarPendientes methods

--- entregas/ponceKevin/src/Liga.java
+++ entregas/ponceKevin/src/Liga.java
@@ -11,17 +11,24 @@ public class Liga {
     }
 
     public void listarPartidos() {
-    
+        System.out.println("Partidos Programados:");
+        for (Partido partido : partidos) {
+            System.out.println(" - " + partido);
+        }
     }
 
     public void mostarPendientes() {
-    
+        System.out.println("Partidos Pendientes:");
+        for (Partido partido : partidos) {
+            if (partido.estaPendiente()) {
+                System.out.println(" - " + partido);
+            }
+        }
     }
 
     public void programarPartido(Partido partido) {
         assert partido != null : "partido no puede ser null";
         partidos.add(partido);
-    
     }
 
     public void registrarResultado() {
