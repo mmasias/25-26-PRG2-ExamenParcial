8fe363efe903b61a55b228ab6b6b875e00716360
Felipe <felipehoyos2008@gmail.com>
2026-03-25 19:47:42 +0100

    Implementar la clase Partido que habia escrito en el examen

--- entregas/hoyosFelipe/src/Partido.java
+++ entregas/hoyosFelipe/src/Partido.java
@@ -2,4 +2,31 @@ package vistas.liga;
 
 class Partido {
 
+    public Partido(Equipo local, Equipo visitante, Fecha fecha){}
+
+    public Partido(Partido partido){}
+
+    public Partido clone(){}
+
+    public Equipo local(){}
+
+    public Equipo visitante(){}
+
+    public Fecha fecha(){}
+
+    public int golesLocal(){}
+
+    public int golesVisitante(){}
+
+    public boolean estaJugando(){}
+
+    public boolean estaPendiente(){}
+
+    public boolean equals(Partido partido){}
+
+    public void registrarResultado(int golesLocal, int golesVisitante){}
+
+    public String toString(){}
+
+    public void mostrar(){}
 }
