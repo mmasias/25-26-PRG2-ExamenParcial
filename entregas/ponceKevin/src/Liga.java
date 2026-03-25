b286645c6f87ceefb0779d5933d08d890f13887b
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 14:32:59 +0000

    refactor: enhance mostrarCalificacion method to display team victories

--- entregas/ponceKevin/src/Liga.java
+++ entregas/ponceKevin/src/Liga.java
@@ -7,7 +7,33 @@ public class Liga {
     }
 
     public void mostrarCalificacion() {
-    
+        System.out.println("Calificacion:");
+        for (Partido partido : partidos) {
+            if (!partido.estaPendiente()) {
+                Equipo equipo = partido.getGanador();
+                boolean yaMostrado = false;
+
+                for (Partido p : partidos) {
+                    if (p == partido) {
+                        break;
+                    }
+                    if (!p.estaPendiente() && p.getGanador() == equipo) {
+                        yaMostrado = true;
+                        break;
+                    }
+                }
+
+                if (!yaMostrado) {
+                    int victorias = 0;
+                    for (Partido p : partidos) {
+                        if (!p.estaPendiente() && p.getGanador() == equipo) {
+                            victorias++;
+                        }
+                    }
+                    System.out.println(" - " + equipo + " | victorias=" + victorias);
+                }
+            }
+        }
     }
 
     public void listarPartidos() {
