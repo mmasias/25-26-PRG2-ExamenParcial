6b103a0f92f09ff8fd379101e632652c0b291240
Felipe <felipehoyos2008@gmail.com>
2026-03-25 19:07:25 +0100

    Corregir error de sintaxis en la creación de objeto Equipo en la clase PruebaLiga
    asigno los nombres a todos los equipos que indica el ejercicio

--- entregas/hoyosFelipe/src/PruebaLiga.java
+++ entregas/hoyosFelipe/src/PruebaLiga.java
@@ -4,6 +4,8 @@ public class PruebaLiga {
     public static void main(String[] args) {
         Liga liga = new Liga("Liga Universitaria Santander 2024/25");
 
-        
+        Equipo cracks = new Equipo("Los Cracks", "Rojo", 8);
+        Equipo byteFC = new Equipo("Byte FC", "Azul", 7;
+        Equipo compiladores = new Equipo("Compiladores", "Verde", 9);
     }
 }
