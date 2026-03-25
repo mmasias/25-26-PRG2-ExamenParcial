40958dfe1ada12e685d0f0d99954569c3085eaf7
luis.hoyos <luis.hoyos1@ct.uneatlantico.es>
2026-03-25 16:15:11 +0100

    Agregar vista publica (INCOMPLETA) Liga

---
+++ entregas/hoyosFelipe/src/Liga.java
@@ -0,0 +1,26 @@
+Package vistas.Liga;
+
+class Liga {
+    public Liga(String nombre) {
+  }
+
+  public Liga() {
+  }
+
+  Public Liga(Liga liga) {
+  }
+
+  Publi Liga clone(){
+
+  }
+
+  Public String nombre() {
+  }
+
+
+
+
+
+
+
+}
