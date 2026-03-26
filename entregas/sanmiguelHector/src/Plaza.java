ca79a03f547b916397f10a0eb252bc182e9efac1
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-26 12:26:50 +0100

    Vista privada del constructor

--- entregas/sanmiguelHector/src/Plaza.java
+++ entregas/sanmiguelHector/src/Plaza.java
@@ -1,6 +1,14 @@
 class Plaza {
     
-    public Plaza (int tipoPlaza, boolean ocupada){}
+    private int tipoPlaza;
+    private boolean ocupada;
+    private String matriculaVehiculo;
+
+
+    public Plaza (int tipoPlaza, boolean ocupada){
+        this.tipoPlaza = tipoPlaza;
+        this.ocupada = ocupada;
+    }
 
     public boolean estaOcupada(){}
 
