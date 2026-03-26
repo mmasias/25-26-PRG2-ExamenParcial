71ebd42f5725ab573934e75ce36cac2144893957
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-26 13:34:30 +0100

    Vista privada del constructor Aparcamiento

--- entregas/sanmiguelHector/src/Aparcamiento.java
+++ entregas/sanmiguelHector/src/Aparcamiento.java
@@ -1,8 +1,25 @@
 class Aparcamiento {
     
+    private String nombreAparcamiento;
+    private Plaza[] plazas;
+    private int totalPlazas;
     
     public Aparcamiento(int numeroPlazasNormal, int numeroPlazasElectrico, int numeroPlazasMoto, String nombreAparcamiento){
+        this.nombreAparcamiento = nombreAparcamiento;
+        this.totalPlazas = numeroPlazasNormal + numeroPlazasElectrico + numeroPlazasMoto;
+        this.plazas = new Plaza[this.totalPlazas];
 
+
+        int i = 0;
+        for (int j = 0; j < numeroPlazasNormal; j++){
+            plazas[i++] = new Plaza(1, false);
+        }
+        for (int j = 0; j < numeroPlazasElectrico; j++) {
+            plazas[i++] = new Plaza(2, false);
+        }
+        for (int j = 0; j < numeroPlazasMoto; j++) {
+            plazas[i++] = new Plaza(3, false);
+        }
     }
 
     public int cantidadPlazasLibres(){}
