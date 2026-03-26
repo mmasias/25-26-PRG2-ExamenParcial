4a8202628351165f78c2863ffe7d1dca2446d73f
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-26 13:24:36 +0100

    Vista pública de la clase Aparcamiento

---
+++ entregas/sanmiguelHector/src/Aparcamiento.java
@@ -0,0 +1,18 @@
+class Aparcamiento {
+    
+    
+    public Aparcamiento(int numeroPlazasNormal, int numeroPlazasElectrico, int numeroPlazasMoto, String nombreAparcamiento){
+
+    }
+
+    public int cantidadPlazasLibres(){}
+
+    public void vehiculoLLego(){}
+
+    public void vehiculoSeFue(){}
+
+    public void mostrarDatos(){}
+
+    public int dineroFacturado(){}
+
+}
