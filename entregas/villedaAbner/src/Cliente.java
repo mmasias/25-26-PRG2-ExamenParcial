4900cd666184a7d7506d6665804fcd8169d3459d
lDraco15 <abnercito.villeda@gmail.com>
2026-03-30 18:53:01 +0200

    Feature: Registra usuarios del sistema en el cliente principal

--- entregas/villedaAbner/src/Cliente.java
+++ entregas/villedaAbner/src/Cliente.java
@@ -11,5 +11,12 @@
         consola.writeln("Libros:");
         consola.writeln("  " + quijote.toString());
         consola.writeln("  " + cienAnios.toString() + "");
+
+        Usuario ana = new Usuario("12345678A", "Ana García", "Direccion no registrada");
+        Usuario juan = new Usuario("87654321B", "Juan Pérez", "Direccion no registrada");
+
+        consola.writeln("Usuarios:");
+        consola.writeln("  " + ana.toString());
+        consola.writeln("  " + juan.toString() + "  ");
     }
 }
