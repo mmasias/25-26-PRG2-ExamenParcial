1edef7847026d0bee329455bb585c3e1838928aa
Maikoledu1303 <maikol.toyo@alumnos.uneatlantico.es>
2026-04-01 08:35:02 +0000

    feature: implementar metodos registrarEntrada y consultarEstado

--- entregas/toyoRoqueMaikolAdrian/src/Aparcamiento.java
+++ entregas/toyoRoqueMaikolAdrian/src/Aparcamiento.java
@@ -1,20 +1,29 @@
 class Aparcamiento{
     private Plaza[] listadoDePlazas;
+    private String[] estadoDePlazas;
 
     public Aparcamiento(int numeroDePlazas){
         this.listadoDePlazas = new Plaza[numeroDePlazas];
+        this.estadoDePlazas = new String[numeroDePlazas];
     };
 
     public Aparcamiento(){
         this.listadoDePlazas = new Plaza[10];
+        this.estadoDePlazas = new String[10];
     };
 
     public void agregarPlaza(int numeroDePlaza, Plaza plaza){
-        this.listadoDePlazas[numeroDePlaza] = plaza;
-    };
+        if(numeroDePlaza >= 0 && numeroDePlaza < this.listadoDePlazas.length) {
+            this.listadoDePlazas[numeroDePlaza] = plaza;
+            this.estadoDePlazas[numeroDePlaza] = plaza.mostrarEstado();
+        }
+    }
 
     public Plaza mostrarPlaza(int numeroDePlaza){
-        return this.listadoDePlazas[numeroDePlaza];
+        if(numeroDePlaza >= 0 && numeroDePlaza < this.listadoDePlazas.length) {
+            return this.listadoDePlazas[numeroDePlaza];
+        }
+        return null;
     }
 
     public int expedirTarifa(int horaDeEntrada, int horaDeSalida, Plaza plaza){
@@ -31,14 +40,35 @@ class Aparcamiento{
 
     public int consultarEspaciosDisponibles(){
         int disponibles = 0;
-        for (Plaza plaza : this.listadoDePlazas) {
-            if (plaza == null) {
+
+        for (String estado : this.estadoDePlazas) {
+            if (estado != null && estado.equals("Disponible")) {
                 disponibles++;
             }
         }
+
         return disponibles;
     }
+    
+    public void registrarEntrada(Plaza plaza, int horaDeEntrada){
+        
+        for (int i = 0; i < this.listadoDePlazas.length; i++) {
+            if (this.listadoDePlazas[i] == plaza) {
+                this.estadoDePlazas[i] = "Ocupado";
+                break;
+            }
+        }
 
-    public void registrarEntrada(Plaza plaza, int horaDeEntrada){};
-    public String consultarEstado(Plaza plaza){};
+    }
+
+    public String consultarEstado(Plaza plaza){
+        
+        for (int i = 0; i < this.listadoDePlazas.length; i++) {
+            if (this.listadoDePlazas[i] == plaza) {
+                return this.estadoDePlazas[i];
+            }
+        }
+        
+        return plaza.mostrarEstado(); 
+    }
 }
