b124dbbaa792f614bc1368973a078bdd51516c8e
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-02 13:21:50 +0200

    clase producto stock mejor

--- entregas/gomezmiguel/src/Producto.java
+++ entregas/gomezmiguel/src/Producto.java
@@ -18,5 +18,18 @@ public class Producto {
     public String verStock() {
         return "Stock actual: " + stock;
     }
+    public String stockActualizado(int cantidad) {
+        this.stock += cantidad;
+        return "Nuevo stock: " + stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public boolean hayStock() {
+        return stock > 0;
+    }
+}
 
     
