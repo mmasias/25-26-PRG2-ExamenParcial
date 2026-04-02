9732d3d4e2f020bf947fa4624a14aa2dbf77ddde
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-02 13:34:41 +0200

    constructor clase tienda online

--- entregas/gomezmiguel/src/TiendaOnline.java
+++ entregas/gomezmiguel/src/TiendaOnline.java
@@ -4,4 +4,9 @@ public class TiendaOnline {
     private Pedido pedido;
     private Fecha fecha;
 
-    
+    public TiendaOnline(Producto producto, Cliente cliente, Pedido pedido, Fecha fecha) {
+        this.productoEnVenta = producto;
+        this.clienteActual = cliente;
+        this.pedidoRealizado = pedido;
+        this.fechaDeOperacion = fecha;
+    }
