d3e03eaac4e6ecf9257fc85c88cc17f31f2c1257
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-02 13:27:37 +0200

    clase pedido para relacionar clase cliente y clase producto

--- entregas/gomezmiguel/src/Pedido.java
+++ entregas/gomezmiguel/src/Pedido.java
@@ -4,3 +4,28 @@ public class Pedido {
     private Fecha fecha;
     private String estado;
 
+    public Pedido(Cliente cliente, Producto producto, Fecha fecha, String estado) {
+        this.cliente = cliente;
+        this.producto = producto;
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public String pedidoRealizado() {
+        return "Pedido de " + producto.mostrarProducto() + " para " + cliente.mostrarCliente();
+    }
+
+    public void quitarStock() {
+        if (producto.hayStock()) {
+            producto.stockActualizado(-1);
+        }
+    }
+
+    public String fechaPedido() {
+        return "Fecha: " + fecha.mostrarFecha();
+    }
+
+    public String verEstado() {
+        return "Estado: " + estado;
+    }
+}
