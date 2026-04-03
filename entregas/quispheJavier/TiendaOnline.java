fb2270b8317e2883e1e85e37d6b92a36c1dac2a3
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2026-04-03 10:54:27 +0200

    se ha agregado la clase producto junto con su vista publica de clases y la vista privada de las mismas

--- entregas/quispheJavier/TiendaOnline.java
+++ entregas/quispheJavier/TiendaOnline.java
@@ -1,5 +1,35 @@
-import java.io.Console;
 
 class TiendaOnline {
+    class Producto {
+    String codigo;
+    String nombreProducto;
+    int precio;
+    int stock;
 
+    public Producto(String codigo, String nombreProducto, int precio, int stock){
+        assert codigo != null;
+        assert nombreProducto != null;
+        assert precio >= 0;
+        assert stock >= 0;
+
+        this.codigo = codigo;
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public Producto(Producto producto){
+        this.codigo = producto.codigo;
+        this.nombreProducto = producto.nombreProducto;
+        this.precio = producto.precio;
+        this.stock = producto.stock;
+    }
+
+    public Producto(){
+        this.codigo = "";
+        this.nombreProducto = "";
+        this.precio = 0;
+        this.stock = 0;
+    }
+}
 }
