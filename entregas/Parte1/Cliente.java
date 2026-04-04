9a29ac4744d63ca53e00df84a2163c8d120dd97b
Dylan Fabian Ruiz Castillo <dylan.ruiz@alumnos.uneatlantico.es>
2026-04-04 12:44:46 +0200

    Defini la clase Cliente con sus atributos principales, junto con un constructor para inicializarlo

--- entregas/Parte1/Cliente.java
+++ entregas/Parte1/Cliente.java
@@ -1,13 +1,25 @@
 package Parte1;
 
 public class Cliente {
-    private String Nombre;
-    private String DNI;
-    private String Direccion;
+    private String nombre;
+    private String dni;
+    private String direccion;
 
-    public Cliente(String Nombre, String DNI, String Direccion);
+    public Cliente(String nombre, String dni, String direccion) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.direccion = direccion;
+    }
 
-    public String getNombre();
-    public String getDni();
-    public String getDireccion();
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
 }
